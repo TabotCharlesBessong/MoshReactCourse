7c035e3c981cafd230e145cc771ac73187dd7e43
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-03-18 14:58:37 +0100

    displaying icons of different game platforms

--- gaming_site/src/components/GameCard.tsx
+++ gaming_site/src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { Game } from "../hooks/useGames";
 import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { PlatformIconList } from ".";
 
 interface GameCardProps {
   game: Game;
@@ -13,6 +14,7 @@ const GameCard: FC<GameCardProps> = ({ game }) => {
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <Heading fontSize="2xl">{game.rating}</Heading>
+        <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
       </CardBody>
     </Card>
   );
--- gaming_site/src/components/index.ts
+++ gaming_site/src/components/index.ts
@@ -1,4 +1,5 @@
-export {default as Navbar} from "./Navbar"
-export {default as ColorModeSwitcher} from "./ColorModeSwitcher"
-export {default as GameGrid} from "./GameGrid"
-export {default as GameCard} from "./GameCard"
+export { default as Navbar } from "./Navbar";
+export { default as ColorModeSwitcher } from "./ColorModeSwitcher";
+export { default as GameGrid } from "./GameGrid";
+export { default as GameCard } from "./GameCard";
+export { default as PlatformIconList } from "./PlatformIconList";
