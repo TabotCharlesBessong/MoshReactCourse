5fbdb49a824b56cb192e5ce5fe745d700d4ab204
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-03-18 08:50:06 +0100

    dark and light mode implementation

---
+++ gaming_site/src/components/ColorModeSwitcher.tsx
@@ -0,0 +1,17 @@
+import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+
+const ColorModeSwitcher = () => {
+  const { toggleColorMode, colorMode } = useColorMode();
+  return (
+    <HStack>
+      <Switch
+        colorScheme="blue"
+        isChecked={colorMode === "dark"}
+        onChange={toggleColorMode}
+      />
+      <Text>{colorMode === "dark" ? "Dark Mode" : "Light Mode"}</Text>
+    </HStack>
+  );
+};
+
+export default ColorModeSwitcher;
