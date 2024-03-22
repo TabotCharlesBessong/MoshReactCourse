b8f144a8f71b65fba9a5b826f44dc2676546146d
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-03-22 17:52:01 +0100

    implementing pagination with react query

--- react_intermediate_ts/src/components/post/PostList.tsx
+++ react_intermediate_ts/src/components/post/PostList.tsx
@@ -3,7 +3,9 @@ import usePosts from "../../hooks/usePosts";
 
 const PostList = () => {
   const [userId, setUserId] = useState<number>();
-  const { data: posts, error, isLoading } = usePosts(userId);
+  const [page, setPage] = useState<number>(1)
+  const pageSize = 10
+  const { data: posts, error, isLoading } = usePosts(userId,{page,pageSize});
   if (isLoading) return <p>Loading....</p>;
   if (error) return <p>{error.message}</p>;
   return (
@@ -19,12 +21,25 @@ const PostList = () => {
         <option value="3">User 3</option>
       </select>
       <ul className="list-group mt-8">
-        {posts?.slice(0, 10)?.map((post) => (
+        {posts?.slice(0, 30)?.map((post) => (
           <li key={post.id} className="list-group-item mt-2">
             {post.title}
           </li>
         ))}
       </ul>
+      <button
+        disabled={page === 1}
+        onClick={() => setPage(page - 1)}
+        className="btn btn-primary my-3"
+      >
+        Previous
+      </button>
+      <button
+        onClick={() => setPage(page + 1)}
+        className="btn btn-primary my-3 ms-4"
+      >
+        Next
+      </button>
     </>
   );
 };
--- react_intermediate_ts/src/hooks/usePosts.ts
+++ react_intermediate_ts/src/hooks/usePosts.ts
@@ -1,22 +1,30 @@
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { CACHE_KEY_POSTS } from "../constants/variables";
 // import postService from "../services/postService";
 import { Todo } from "../types";
 import axios from "axios";
 
-const usePosts = (userId: number | undefined) => {
+interface PostQuery {
+  page:number
+  pageSize:number
+}
+
+const usePosts = (userId: number | undefined,query:PostQuery) => {
   return useQuery<Todo[], Error>({
-    queryKey:userId ? ["users", userId, CACHE_KEY_POSTS] : [CACHE_KEY_POSTS],
+    queryKey:userId ? ["users", userId, CACHE_KEY_POSTS,query] : [CACHE_KEY_POSTS,query],
     // queryFn: postService.getAll,
     queryFn: () =>
       axios
         .get("https://jsonplaceholder.typicode.com/posts", {
           params: {
             userId,
+            _start:(query.page - 1) * query.pageSize,
+            _limit:query.pageSize
           },
         })
         .then((res) => res.data),
     staleTime: 10 * 100,
+    placeholderData:keepPreviousData
   });
 };
 
